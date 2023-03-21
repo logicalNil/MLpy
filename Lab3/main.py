55536d3e37c3a18bfaaa2dacafb4fb1c4b068a49
Nilay Ranjan <80938367+logicalNil@users.noreply.github.com>
2023-03-21 08:02:59 +0000

    Lab3

---
+++ Lab3/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+# Read the data
+df = pd.read_csv('iris.csv')
+
+print("Number of columns present:-",
+      len(df.columns))
+print("Number of rows present:-", 
+      len(df.index))
+print(df.dtypes)
+
+print(df.head(5))
+
+item_cou = df["species"].value_counts()
+print(item_cou)
+
+print(df["species"].unique())
+
+# remove versicolor from iris dataset
+versi = df[df["species"] != "versicolor"]
+print(versi)
+
+# remove species column from versi
+new = versi.drop("species", axis=1)
+print(new)
+
